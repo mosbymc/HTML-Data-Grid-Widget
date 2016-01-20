c4383188956a8d0766b546263e78fff117cf6471
mosbymc <mosbymc@users.noreply.github.com>
2016-01-20 06:44:05 -0500

    Update gridData.js

--- gridData.js
+++ gridData.js
@@ -1,4 +1,6 @@
 var gridData = {
+	useValidator: true,
+	useFormatter: true,
 	sortable: true,
 	reorderable: true,
 	groupable: true,
@@ -6,14 +8,40 @@ var gridData = {
 	pagingOptions: [25, 50, 100],
 	//pageSize: 50,
 	summaryRow: {
-		Service: "count",
-		Labor: "average",
-		Cost: "max",
-		Date: "",
-		Paid: "count",
-		Customer: "",
-		Billed: "total",
+		Service: {
+			type: "count"
+		},
+		Labor: {
+			type: "average"
+		},
+		Cost: {
+			type: "max",
+			template: "${{data}}"
+		},
+		Date: {
+			type: ""
+		},
+		Time: {
+			type: ""
+		},
+		Paid: {
+			type: "count"
+		},
+		Customer: {
+			type: ""
+		},
+		Billed: {
+			type: "total",
+			template: "${{data}}"
+		},
+		Markup: {
+			type: ""
+		},
 		positionAt: "top"
+	},
+	rows: {
+		alternateRows: ["testAlt"],
+		all: ["testAll"]
 	},
 	columns: {
 		Service: {
@@ -28,35 +56,106 @@ var gridData = {
 			width: 180
 		},
 		Labor: {
-			type: "time",
-			measurement: "hours",
+			type: "number",
 			template: "{{data}} hour(s)",
+			decimals: 0,
 			selectable: true,
 			options: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-			width: 160
+			width: 160,
+			validation: {
+				required: true,
+				customRules: {
+					maxValue: function maxValue(callback) {
+						if ($(this).val() >= 10)
+							callback(false, "You must select a lower value.", 150);
+						else
+							callback(true);
+					}
+				}
+			}
 		},
 		Cost: {
-			type: "currency",
+			type: "number",
 			symbol: "$",
 			template: "${{data}}",
 			filterable: true,
 			editable: true,
 			min: 1,
 			max: 10000,
-			width: 180
+			width: 180,
+			validation: {
+				required: true,
+				customRules: {
+					maxCost: function maxCost(callback) {
+						if (this.value > 1500)
+							callback(false, "Total cost cannot exceed $1500.00.", 200);
+						else
+							callback(true);
+					},
+					minCost: function minCost(callback) {
+						if (this.value > 1500)
+							callback(false, "Total cost cannot be less than $1500.00.", 200);
+						else
+							callback(true);
+					}
+				}
+			}
 		},
 		Billed: {
-			type: "currency",
+			type: "number",
 			symbol: "$",
 			template: "${{data}}",
-			width: 125
+			width: 125,
+			validation: {
+				required: true,
+				customRules: {
+					maxBilled: function maxBilled(callback) {
+						if (this.value > 1500)
+							callback(false, "Total billing cannot exceed $1500.00.", 200);
+						else
+							callback(true);
+					}
+				}
+			}
+		},
+		Markup: {
+			filterable: false,
+			type: "number",
+			width: 140,
+			editable: true,
+			title: "Parts Markup",
+			template: "{{data}}%",
+			decimals: 0
 		},
 		Date: {
 			filterable: true,
 			type: "date",
 			width: 180,
 			editable: true,
-			format: "mm/dd/yyyy"
+			format: "mm/dd/yyyy",
+			validation: {
+				required: true,
+				customRules: {
+					dateValidation: function dateValidation(callback) {
+						var userDate = new Date($(this).val());
+						var timezone = userDate.getTimezoneOffset();
+						var trueTime = new Date(userDate.getTime() + (timezone*60000));
+						if (trueTime > new Date())
+							callback(false, "You cannot select a future date.", 200);
+						else
+							callback(true);
+					}
+				}
+			}
+		},
+		Time: {
+			title: "Check-in Time",
+			filterable: true,
+			type: "time",
+			width: 150,
+			editable: true,
+			timeFormat: "12",
+			delimiter: ":"
 		},
 		Paid: {
 			type: "bool",
@@ -71,27 +170,36 @@ var gridData = {
 		data: [
 			{
 				Service: "New Brakes",
+				Date: "10/18/1982",
 				Labor: "2",
-				Cost: "55.00",
+				Cost: 55.00,
 				Paid: false,
 				Customer: "Mark Mosby",
-				Billed: "75.00"
+				Billed: "75.00",
+				Markup: 150,
+				Time: "12:15:32 AM"
 			},
 			{
 				Service: "Tire Rotation",
+				Date: "12/12/2012",
 				Labor: "1",
 				Cost: "40.00",
 				Paid: true,
 				Customer: "David Hill",
-				Billed: "65.00"
+				Billed: "65.00",
+				Markup: 100,
+				Time: "3:45:10 PM"
 			},
 			{
 				Service: "New Transmission",
+				Date: "1/15/2016",
 				Labor: "12",
 				Cost: "800.00",
 				Paid: true,
 				Customer: "Lenny Brensman",
-				Billed: "1010.00"
+				Billed: "1010.00",
+				Markup: 200,
+				Time: "9:03:48 AM"	
 			},
 			{
 				Service: "Oil Change",
@@ -100,55 +208,75 @@ var gridData = {
 				Cost: "55.00",
 				Paid: true,
 				Customer: "Dustin Waldroup",
-				Billed: "75.00"
+				Billed: "75.00",
+				Markup: 50,
+				Time: "8:57:34 AM"
 			},
 			{
 				Service: "New Brakes",
+				Date: "11/25/2015",
 				Labor: "1.5",
 				Cost: "45.00",
 				Paid: true,
 				Customer: "Leyth Gorgeis",
-				Billed: "55.00"
+				Billed: "55.00",
+				Markup: 67,
+				Time: "10:10:12: AM"
 			},
 			{
 				Service: "New Clutch",
+				Date: "10/12/2015",
 				Labor: "5",
 				Cost: "200.00",
 				Paid: true,
 				Customer: "Kendra Billings",
-				Billed: "250.00"
+				Billed: "250.00",
+				Markup: 108,
+				Time: "4:31:59 AM"
 			},
 			{
 				Service: "Oil Change",
+				Date: "12/01/2015",
 				Labor: "0.5",
 				Cost: "70.00",
 				Paid: true,
 				Customer: "Callie McConnell",
-				Billed: "90.00"
+				Billed: "90.00",
+				Markup: 93,
+				Time: "5:05:36 AM"
 			},
 			{
 				Service: "New Spark Plugs",
+				Date: "11/18/2014",
 				Labor: "0.5",
 				Cost: "65.00",
 				Paid: true,
 				Customer: "Moshe Adnesik",
-				Billed: "90.00"
+				Billed: "90.00",
+				Markup: 100,
+				Time: "11:24:56 AM"
 			},
 			{
 				Service: "Emission Check",
+				Date: "1/25/2015",
 				Labor: "0.25",
 				Cost: "25.00",
 				Paid: true,
 				Customer: "Jim Song",
-				Billed: "30.00"
+				Billed: "30.00",
+				Markup: 500,
+				Time: "9:49:02 AM"
 			},
 			{
 				Service: "New Exhaust",
+				Date: "9/07/2016",
 				Labor: "2",
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Bob Hollister",
-				Billed: "295.00"
+				Billed: "295.00",
+				Markup: 67,
+				Time: "5:11:30 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -157,7 +285,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Michael Villareal",
-				Billed: "260.00"
+				Billed: "260.00",
+				Markup: 32,
+				Time: "8:08:00 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -166,7 +296,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Bill Landers",
-				Billed: "282.00"
+				Billed: "282.00",
+				Markup: 900,
+				Time: "11:01:53 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -175,7 +307,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Lou Bennet",
-				Billed: "350.00"
+				Billed: "350.00",
+				Markup: 225,
+				Time: "4:00:30 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -184,7 +318,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Erik Ciccarone",
-				Billed: "299.00"
+				Billed: "299.00",
+				Markup: 108,
+				Time: "2:20:46 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -193,7 +329,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "John Mitchell",
-				Billed: "265.00"
+				Billed: "265.00",
+				Markup: 200,
+				Time: "1:39:00 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -202,7 +340,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Paul Rogers",
-				Billed: "283.00"
+				Billed: "283.00",
+				Markup: 100,
+				Time: "6:06:32 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -211,7 +351,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Jane Mosby",
-				Billed: "290.00"
+				Billed: "290.00",
+				Markup: 67,
+				Time: "8:49:00 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -220,7 +362,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Mark Mosby",
-				Billed: "75.00"
+				Billed: "75.00",
+				Markup: 5,
+				Time: "9:42:31 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -229,7 +373,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "David Hill",
-				Billed: "65.00"
+				Billed: "65.00",
+				Markup: 37,
+				Time: "4:07:56 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -238,7 +384,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Lenny Brensman",
-				Billed: "1010.00"
+				Billed: "1010.00",
+				Markup: 98,
+				Time: "8:02:20 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -247,7 +395,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Dustin Waldroup",
-				Billed: "75.00"
+				Billed: "75.00",
+				Markup: 52,
+				Time: "10:06:50 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -256,7 +406,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Leyth Gorgeis",
-				Billed: "55.00"
+				Billed: "55.00",
+				Markup: 67,
+				Time: "3:23:33 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -265,7 +417,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Kendra Billings",
-				Billed: "250.00"
+				Billed: "250.00",
+				Markup: 100,
+				Time: "9:56:01 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -274,7 +428,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Callie McConnell",
-				Billed: "90.00"
+				Billed: "90.00",
+				Markup: 200,
+				Time: "11:51:01 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -283,7 +439,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Moshe Adnesik",
-				Billed: "90.00"
+				Billed: "90.00",
+				Markup: 250,
+				Time: "1:47:21 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -292,7 +450,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Jim Song",
-				Billed: "30.00"
+				Billed: "30.00",
+				Markup: 87,
+				Time: "5:05:39 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -301,7 +461,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Bob Hollister",
-				Billed: "295.00"
+				Billed: "295.00",
+				Markup: 233,
+				Time: "3:03:03 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -310,7 +472,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Michael Villareal",
-				Billed: "260.00"
+				Billed: "260.00",
+				Markup: 122,
+				Time: "2:35:49 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -319,7 +483,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Bill Landers",
-				Billed: "282.00"
+				Billed: "282.00",
+				Markup: 868,
+				Time: "12:56:31 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -328,7 +494,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Lou Bennet",
-				Billed: "350.00"
+				Billed: "350.00",
+				Markup: 134,
+				Time: "11:20:26 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -337,7 +505,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Erik Ciccarone",
-				Billed: "299.00"
+				Billed: "299.00",
+				Markup: 72,
+				Time: "2:33:54 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -346,7 +516,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "John Mitchell",
-				Billed: "265.00"
+				Billed: "265.00",
+				Markup: 189,
+				Time: "12:12:12 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -355,7 +527,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Paul Rogers",
-				Billed: "283.00"
+				Billed: "283.00",
+				Markup: 23,
+				Time: "1:09:15 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -364,7 +538,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Jane Mosby",
-				Billed: "290.00"
+				Billed: "290.00",
+				Markup: 39,
+				Time: "4:51:06 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -373,7 +549,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Courtney Wilson",
-				Billed: "300.00"
+				Billed: "300.00",
+				Markup: 169,
+				Time: "8:54:31 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -382,7 +560,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Aaron Schwan",
-				Billed: "310.00"
+				Billed: "310.00",
+				Markup: 100,
+				Time: "9:15:04 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -391,7 +571,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Marc Smith",
-				Billed: "265.00"
+				Billed: "265.00",
+				Markup: 102,
+				Time: "12:32:09 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -400,7 +582,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Greg Main",
-				Billed: "250.00"
+				Billed: "250.00",
+				Markup: 67,
+				Time: "3:45:32 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -409,7 +593,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Some Dude",
-				Billed: "5000.00"
+				Billed: "5000.00",
+				Markup: 433,
+				Time: "5:31:08 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -418,7 +604,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Some Chick",
-				Billed: "5000.00"
+				Billed: "5000.00",
+				Markup: 235,
+				Time: "9:45:16 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -427,7 +615,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Bert",
-				Billed: "1.00"
+				Billed: "1.00",
+				Markup: 45,
+				Time: "11:22:59 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -436,7 +626,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Ernie",
-				Billed: "2.00"
+				Billed: "2.00",
+				Markup: 67,
+				Time: "3:33:33 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -445,7 +637,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Phillip J. Fry",
-				Billed: "282.00"
+				Billed: "282.00",
+				Markup: 222,
+				Time: "10:18:43 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -454,7 +648,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Professor Huebert Farnsworth",
-				Billed: "300.00"
+				Billed: "300.00",
+				Markup: 111,
+				Time: "8:18:47 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -463,7 +659,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Cuebert Farnsworth",
-				Billed: "310.00"
+				Billed: "310.00",
+				Markup: 123,
+				Time: "10:04:15 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -472,7 +670,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Robot Devil",
-				Billed: "300.00"
+				Billed: "300.00",
+				Markup: 456,
+				Time: "12:47:32 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -481,7 +681,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Billionaire Bot",
-				Billed: "280.00"
+				Billed: "280.00",
+				Markup: 623,
+				Time: "2:56:01 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -490,7 +692,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "9",
-				Billed: "400.00"
+				Billed: "400.00",
+				Markup: 133,
+				Time: "5:13:50 PM"
 			},
 			{
 				Service: "New Row Test",
@@ -499,7 +703,9 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Hanukkah Zombie",
-				Billed: "385.00"
+				Billed: "385.00",
+				Markup: 12,
+				Time: "9:38:56 AM"
 			},
 			{
 				Service: "New Row Test",
@@ -508,23 +714,31 @@ var gridData = {
 				Cost: "250.00",
 				Paid: true,
 				Customer: "Harold Zoid",
-				Billed: "260.00"
+				Billed: "260.00",
+				Markup: 36,
+				Time: "9:56:02 AM"
 			},
 			{
 				Service: "Tire Rotation",
+				Date: "6/23/2010",
 				Labor: "1",
 				Cost: "40.00",
 				Paid: true,
 				Customer: "Mayor Poopinmayer",
-				Billed: "290.00"
+				Billed: "290.00",
+				Markup: 45,
+				Time: "10:33:15 AM"
 			},
 			{
 				Service: "New Transmission",
+				Date: "5/06/2013",
 				Labor: "12",
 				Cost: "800.00",
 				Paid: true,
 				Customer: "Yancey Fry",
-				Billed: "280.00"
+				Billed: "280.00",
+				Markup: 56,
+				Time :"1:15:08 PM"
 			},
 			{
 				Service: "Oil Change",
@@ -533,15 +747,20 @@ var gridData = {
 				Cost: "55.00",
 				Paid: true,
 				Customer: "Hermes Conrad",
-				Billed: "275.00"
+				Billed: "275.00",
+				Markup: 245,
+				Time: "4:00:13 PM"
 			},
 			{
 				Service: "New Brakes",
+				Date: "8/16/2012",
 				Labor: "1.5",
 				Cost: "45.00",
 				Paid: true,
 				Customer: "Roberto",
-				Billed: "-1000.00"
+				Billed: "-1000.00",
+				Markup: 23,
+				Time: "5:55:01 PM"
 			}
 		]
 	}
