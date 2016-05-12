3903ac5eccd9049d537618a3130f1b01767eb42d
mosbymc <mosbymc@gmail.com>
2016-05-12 07:00:03 -0400

    develop: added more integration tests

--- tests/integrationTests/grid.validation.test.js
+++ tests/integrationTests/grid.validation.test.js
@@ -10,7 +10,7 @@ var qunitFixture = '#qunit-fixture',
 //Tests based around successful grid creation
 QUnit.module('Successful grid validation tests', {
     beforeEach: function destroyAndRecreateGrid() {
-        var tmp = $("#qunit-fixture")[0];
+        var tmp = $(qunitFixture)[0];
         if (tmp.grid)
             tmp.grid.destroy();
         grid.createGrid(gridData, document.getElementById("qunit-fixture"));
@@ -18,7 +18,8 @@ QUnit.module('Successful grid validation tests', {
 });
 
 QUnit.test('General grid API tests', function gridEventTests(assert) {
-    var done = assert.async();
+    assert.expect(6);
+    var done = assert.async(6);
     var gridApi = $(qunitFixture)[0].grid;
 
     var costCol = $(qunitFixture).find(contentDiv).find('table').find('tr').first().find('[data-field="Cost"]');
@@ -28,26 +29,28 @@ QUnit.test('General grid API tests', function gridEventTests(assert) {
     costInput.blur();
 
     setTimeout(function timeoutCallback() {
-        var costCol = $(qunitFixture).find(contentDiv).find('table').find('tr').first().find('[data-field="Cost"]');
-        var costInput = costCol.children('input');
-        var t = $(qunitFixture).find(contentDiv).find('table').find('tr').find('td :nth-child(4)')[0].click();
-        var cellVal = gridApi.activeCellData;
-        assert.ok(gridApi.activeCellData === 'New Brakes', 'New Brakes was returned when the first rows first column was clicked');
-        assert.ok(gridApi.selectedRow === 0, 'The selected cell\'s parent row index was returned');
-        var cell = $(qunitFixture).find(contentDiv).find('table').find('tr').find('td').first()[0];
-        var colIndex = $(cell).parents(".grid-wrapper").find(".grid-header-wrapper").find(".grid-headerRow").children("[data-field='" + $(cell).data("field") + "']").data("index");
-        assert.deepEqual(gridApi.selectedColumn, { field: $(cell).data("field"), columnIndex: colIndex }, 'null was returned when no active cell');
-        done();
-    }, 0);
-    /*
-    var colIndex = cell.parents('.grid-wrapper').find('.grid-header-wrapper').find('.grid-headerRow').children('[data-field="' + field + '"]').data('index');
+        assert.ok(costInput.hasClass('invalid'), 'The input is in an invalid state.');
+        assert.ok($(document).find('.errorMessage').length === 1, 'There is one error message.');
 
-    var t = $(qunitFixture).find(contentDiv).find('table').find('tr').find('td :nth-child(4)')[0].click();
-    var cellVal = gridApi.activeCellData;
-    assert.ok(gridApi.activeCellData === 'New Brakes', 'New Brakes was returned when the first rows first column was clicked');
-    assert.ok(gridApi.selectedRow === 0, 'The selected cell\'s parent row index was returned');
-    var cell = $(qunitFixture).find(contentDiv).find('table').find('tr').find('td').first()[0];
-    var colIndex = $(cell).parents(".grid-wrapper").find(".grid-header-wrapper").find(".grid-headerRow").children("[data-field='" + $(cell).data("field") + "']").data("index");
-    assert.deepEqual(gridApi.selectedColumn, { field: $(cell).data("field"), columnIndex: colIndex }, 'null was returned when no active cell');
-    */
+        costInput.val('1800');
+        costInput.blur();
+
+        setTimeout(function nestedTimeoutCallback() {
+            assert.ok(costInput.hasClass('invalid'), 'The input is in an invalid state.');
+            assert.ok($(document).find('.errorMessage').length === 2, 'There is one error message.');
+
+            costInput.val('10');
+            costInput.blur();
+
+            setTimeout(function secondNestedTimeoutCallback() {
+                assert.ok(!costInput.hasClass('invalid'), 'The input is in a valid state.');
+                assert.ok($(document).find('.errorMessage').length === 0, 'There are no error messages.');
+
+                /*var tmp = $("#qunit-fixture")[0];
+                if (tmp.grid)
+                    tmp.grid.destroy();*/
+                done();
+            }, 0);
+        }, 0);
+    }, 0);
 });
