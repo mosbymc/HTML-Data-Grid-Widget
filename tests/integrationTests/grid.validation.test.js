0e59c7f43dc0dba455a02fa3d63d364bfb099a53
mosbymc <mosbymc@gmail.com>
2016-05-12 21:54:50 -0400

    develop: added more integration tests.

--- tests/integrationTests/grid.validation.test.js
+++ tests/integrationTests/grid.validation.test.js
@@ -5,7 +5,8 @@
  */
 
 var qunitFixture = '#qunit-fixture',
-    contentDiv = '.grid-content-div';
+    contentDiv = '.grid-content-div',
+    errorMessage = '.errorMessage';
 
 //Tests based around successful grid creation
 QUnit.module('Successful grid validation tests', {
@@ -17,10 +18,9 @@ QUnit.module('Successful grid validation tests', {
     }
 });
 
-QUnit.test('General grid API tests', function gridEventTests(assert) {
-    assert.expect(6);
-    var done = assert.async(6);
-    var gridApi = $(qunitFixture)[0].grid;
+QUnit.test('Validator integration tests', function gridEventTests(assert) {
+    assert.expect(9);
+    var done = assert.async(9);
 
     var costCol = $(qunitFixture).find(contentDiv).find('table').find('tr').first().find('[data-field="Cost"]');
     costCol[0].click();
@@ -29,22 +29,29 @@ QUnit.test('General grid API tests', function gridEventTests(assert) {
     costInput.blur();
 
     setTimeout(function timeoutCallback() {
+        var error = $(document).find(errorMessage);
+
         assert.ok(costInput.hasClass('invalid'), 'The input is in an invalid state.');
-        assert.ok($(document).find('.errorMessage').length === 1, 'There is one error message.');
+        assert.ok(error.length === 1, 'There is one error message.');
+        assert.equal(error.text(), 'Required field.');
+
 
         costInput.val('1800');
         costInput.blur();
 
         setTimeout(function nestedTimeoutCallback() {
+            error = $(document).find(errorMessage);
             assert.ok(costInput.hasClass('invalid'), 'The input is in an invalid state.');
-            assert.ok($(document).find('.errorMessage').length === 2, 'There is one error message.');
+            assert.ok(error.length === 2, 'There is one error message.');
+            assert.equal(error[0].innerText, 'Total cost cannot exceed $1500.00.');
+            assert.equal(error[1].innerText, 'Total cost cannot be less than $1500.00.');
 
             costInput.val('10');
             costInput.blur();
 
             setTimeout(function secondNestedTimeoutCallback() {
                 assert.ok(!costInput.hasClass('invalid'), 'The input is in a valid state.');
-                assert.ok($(document).find('.errorMessage').length === 0, 'There are no error messages.');
+                assert.ok($(document).find(errorMessage).length === 0, 'There are no error messages.');
 
                 var tmp = $("#qunit-fixture")[0];
                 if (tmp.grid)
