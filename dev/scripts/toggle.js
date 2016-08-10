a53c6fe4ca43d091913c8793d4667d175969ac73
mosbymc <mosbymc@gmail.com>
2016-08-10 07:39:57 -0400

    multi-group: more work on toggler

--- dev/scripts/toggle.js
+++ dev/scripts/toggle.js
@@ -5,127 +5,48 @@ function toggle(element, options) {
     options.direction = options.direction || 'horizontal';
     options.callback = typeof options.callback === 'function' ? options.callback : function noop(){};
     options.slideDistance = options.direction === 'horizontal' ? element.width() : element.height();
+    options.startTime = Date.now();
+    var elemOffsets = element.offset();
+    options.startLoc = options.direction === 'horizontal' ? (elemOffsets.left || parseFloat(element[0].style.left.replace('px', '')))
+        : (elemOffsets.top || parseFloat(element[0].style.top.replace('px', '')));
     var elementStyles = element[0].ownerDocument.defaultView.getComputedStyle(element[0], null);
-    options.open = elementStyles.getPropertyValue('display') || elementStyles['display'] === 'none' ? true : false;
-    //computed.getPropertyValue( name ) || computed[ name ] : undefined;
+    var t = elementStyles.getPropertyValue('display') || elementStyles['display'];
+    options.open = t === 'none';
+    element.css('display', 'block');
+    element.css('overflow', 'hidden');
 
-    if (element.css('display') === 'none') {
-        slideOut(element, options);
-    }
-    else {
-        slideIn(element, options);
-    }
+    animate(element, options, options.duration, options.startTime);
 
-    function slideOut(elem, options) {
-        animate(elem, options, Date.now());
-    }
-
-    function slideIn(elem, options) {
-        animate(elem, options, Date.now());
-    }
-
-    function animate(elem, options, prevTick) {
-        var curTick = Date.now();
-        var elapsedTicks = curTick - prevTick;
-
-        var opening = (oID == '') ? null : document.getElementById(oID);
-        var closing = (cID == '') ? null : document.getElementById(cID);
+    function animate(elem, options, timeRemaining, startTick) {
+        var curTick = Date.now(),
+            elapsedTicks = curTick - startTick,
+            style = options.direction === 'horizontal' ? 'left' : 'top';
 
         if(options.duration <= elapsedTicks) {
-            if(opening != null)
-                opening.style.height = ContentHeight + 'px';
-
-            if(closing != null) {
-                closing.style.display = 'none';
-                closing.style.height = '0px';
+            if(options.open) {
+                elem.css(style, (options.startLoc + options.slideDistance));
+                elem.css('overflow', 'auto');
+            }
+            else {
+                elem.css(style, (options.startLoc - options.slideDistance));
+                elem.css('display', 'none');
             }
             return;
         }
 
-        timeLeft -= elapsedTicks;
-        var newClosedHeight = Math.round((timeLeft/TimeToSlide) * ContentHeight);
+        timeRemaining -= elapsedTicks;
+        var newSlidePos = Math.round((elapsedTicks/options.duration) * options.slideDistance);
 
-        if(opening != null) {
-            if(opening.style.display != 'block')
-                opening.style.display = 'block';
-            opening.style.height = (ContentHeight - newClosedHeight) + 'px';
+        if(options.open) {
+            console.log('StepOut: ' + (options.startLoc - options.slideDistance + newSlidePos + elem.width()))
+            elem.css(style, options.startLoc - options.slideDistance + newSlidePos + elem.width());
         }
-
-        if(closing != null)
-            closing.style.height = newClosedHeight + 'px';
-
-        setTimeout("animate(" + curTick + "," + timeLeft + ",'"
-            + cID + "','" + oID + "')", 33);
-    }
-}
-
-function openTab(divID, height) {
-    var openingHeader = $("#" + divID + "Header");
-    var closingHeader = $("#" + openAccordion + "Header");
-    //var openingSpan = document.getElementById(divID + "Span");
-    //var closingSpan = document.getElementById(openAccordion + "Span");
-
-    if(openAccordion == divID) {
-        divID = null;
-        closingHeader.addClass("closeHeader");
-        closingHeader.removeClass("openHeader");
-        //closingSpan.className = "downArrow";
-    }
-    if (openAccordion == null) {
-        openingHeader.addClass("openHeader");
-        openingHeader.removeClass("closeHeader");
-        //openingSpan.className = "upArrow";
-    }
-
-    ContentHeight = height;
-
-    setTimeout("animate(" + new Date().getTime() + "," + TimeToSlide + ",'"
-        + openAccordion + "','" + divID + "')", 33);
-
-    openingHeader.removeClass("closeHeader");
-    openingHeader.addClass("openHeader");
-    closingHeader.removeClass("openHeader");
-    closingHeader.addClass("closeHeader");
-    openAccordion = divID;
-}
-
-function animate (lastTick, timeLeft, cID, oID) {
-    var openingHeader = document.getElementById(oID + "Header");
-    var closingHeader = document.getElementById(cID + "Header");
-    var openingSpan = document.getElementById(oID + "Span");
-    var closingSpan = document.getElementById(cID + "Span");
-
-    var curTick = new Date().getTime();
-    var elapsedTicks = curTick - lastTick;
-
-    var opening = (oID == '') ? null : document.getElementById(oID);
-    var closing = (cID == '') ? null : document.getElementById(cID);
-
-    if(timeLeft <= elapsedTicks) {
-        if(opening != null)
-            opening.style.height = ContentHeight + 'px';
-
-        if(closing != null) {
-            closing.style.display = 'none';
-            closing.style.height = '0px';
+        else {
+            console.log('StepIn: ' + (options.startLoc + options.slideDistance - newSlidePos - elem.width()));
+            elem.css(style, options.startLoc + options.slideDistance - newSlidePos - elem.width());
         }
-        return;
+        setTimeout(function animateTimer() {
+            animate(elem, options, timeRemaining, startTick);
+        }, 33);
     }
-
-    timeLeft -= elapsedTicks;
-    var newClosedHeight = Math.round((timeLeft/TimeToSlide) * ContentHeight);
-
-    if(opening != null) {
-        if(opening.style.display != 'block')
-            opening.style.display = 'block';
-        opening.style.height = (ContentHeight - newClosedHeight) + 'px';
-    }
-
-    if(closing != null)
-        closing.style.height = newClosedHeight + 'px';
-
-    setTimeout("animate(" + curTick + "," + timeLeft + ",'"
-        + cID + "','" + oID + "')", 33);
-    //openingSpan.className = "upArrow";
-    //closingSpan.className = "downArrow";
 }
