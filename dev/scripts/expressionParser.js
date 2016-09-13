63a5801f58818e06c729c10790ddf63f6d2a9852
mosbymc <mosbymc@gmail.com>
2016-09-13 06:56:01 -0400

    develop: updated expression parser; added babel for es6 conversion

--- dev/scripts/expressionParser.js
+++ dev/scripts/expressionParser.js
@@ -9,7 +9,7 @@
  - (AND|OR|NOT|XOR|\\|\\||^|&&|!)
  - (AND|OR|NOT|XOR)
 
-    AND                    NAND
+ AND                    NAND
  ∧ | 0 | 1             !∧ | 0 | 1
  ------------          ------------
  0 | 0 | 0              0 | 1 | 1
@@ -17,14 +17,14 @@
  1 | 0 | 1              1 | 1 | 0
 
 
-    OR                      NOR
+ OR                      NOR
  ∨ | 0 | 1             !∨ | 0 | 1
  ------------          ------------
  0 | 0 | 1              0 | 1 | 0
  ------------          ------------
  1 | 1 | 1              1 | 0 | 0
 
-    XOR                    XNOR
+ XOR                    XNOR
  ⊕ | 0 | 1             !⊕ | 0 | 1
  ------------          -------------
  0 | 0 | 1               0 | 1 | 0
@@ -32,18 +32,45 @@
  1 | 1 | 0               1 | 0 | 1
 
 
-AND: p && q = If p, then p AND q is q, otherwise p AND q is p
-OR: p || q = If p, then p OR q is p, otherwise p OR q is q
-NAND = !(p AND q) = !p OR !q
-NOR = !(p OR q) = !p AND !q
+ AND: p && q = If p, then p AND q is q, otherwise p AND q is p
+ OR: p || q = If p, then p OR q is p, otherwise p OR q is q
+ NAND = !(p AND q) = !p OR !q
+ NOR = !(p OR q) = !p AND !q
 
 
-Post Evaluation
-c1.field1 >= someVal AND c1.field1 != c1.field2 OR c2.field1 <= c1.field1
+ Post Evaluation
+ c1.field1 >= someVal AND c1.field1 != c1.field2 OR c2.field1 <= c1.field1
 
-Inline Evaluation
-12 >= 15 AND 12 != 8 OR 6 <= 12
+ Inline Evaluation
+ 12 >= 15 AND 12 != 8 OR 6 <= 12
 
+
+
+ c1.field1 > x && c1.field1 < y && c1.field2 !== c2.field1 || (c1.field4 === z ^ a > b) && true
+
+
+
+ - Parse expression string on boolean operators to get each token
+ - If this is a pre-eval
+    - If the token is an operand
+        - Parse and evaluate the result(s)
+        - Evaluate each statement of truth
+            - If the statement just has inlined values, can evaluate immediately
+            - If the statement has variables or object properties, ensure access and gather values to inline
+            - If the statement has a function, start at global level and work down until function is found and executed; inline result
+        - Push result of evaluation onto queue
+    - If the token is an operator
+        - Use shunting-yard algorithm to determine where it belongs
+    - If the token is a paren
+        - Use shunting-yard algorithm
+    - If this is a post-eval
+        - If the token is an operand
+            - Parse the token and create a new boolean expression object; set appropriate values
+            - Then push the boolean expression object into the queue
+        - If the token is an operator
+            - Use shunting-yard algorithm to determine where it belongs
+        - If the token is a paren
+            - Use shunting-yard algorithm
 
  */
 
@@ -109,6 +136,15 @@ function comparator(val, base, type) {
             return !!~val.toLowerCase().indexOf(base.toLowerCase());
         case 'nct':
             return !~val.toLowerCase().indexOf(base.toLowerCase());
+    }
+}
+
+function getNumberOfOperands(operator) {
+    switch (operator) {
+        case '!':
+            return 1;
+        default:
+            return 2;
     }
 }
 
@@ -207,6 +243,58 @@ var parser = {
     }
 };
 
+var rootNode = {
+    init: function _init(config) {
+        this.field1 = config.field1 || null;
+        this.field2 = config.field2 || null;
+        this.value1 = config.value1 || null;
+        this.value2 = config.value2 || null;
+        this.operator = config.operator || null;
+
+        if (this.operator) {
+            this.numberOfChildren = getNumberOfOperands(this.operator);
+            this.children = [];
+        }
+    },
+    addChild: function _addChild(child) {
+        if (this.children && this.children.length < this.numberOfChildren)
+            this.children.push(child);
+    },
+    get context1() {
+        return this._context1;
+    },
+    set context1(val) {
+        this._context1 = val;
+    },
+    get context2() {
+        return this._context2;
+    },
+    set context2(val) {
+        this._context2 = val;
+    }
+};
+
+var childNode = Object.create(rootNode);
+
+Object.defineProperties(rootNode, {
+    'evaluate': {
+        value: function _evaluate() {
+
+        },
+        writable: false,
+        configurable: false
+    },
+    'val': {
+
+    },
+    'base': {
+
+    },
+    'operator': {
+
+    }
+});
+
 var expression = {
     init: function _init(config) {
         this.field1 = config.field1 || null;
@@ -235,6 +323,32 @@ Object.defineProperties(expression, {
 
     }
 });
+
+var preEvalNode = {
+    init: function _init(nodeType, value) {
+        this.type = nodeType;
+        this.value = value;
+
+        if (this.type === 'operator') {
+            this.children = [];
+            this.numOperands = getNumberOfOperands(value);
+        }
+
+        return this;
+    },
+    addChild: function _addChild(childNode) {
+        if (preEvalNode.isPrototypeOf(childNode) && this.numOperands < this.children.length)
+            this.children.push(childNode);
+        return this;
+    },
+    evaluate: function _evaluate() {
+        if (this.type === 'operand')
+            return this.value;
+        else {
+            return comparator(this.children[0], this.children[1], this.value);
+        }
+    }
+};
 
 var expressionParser = {
     init: function _init(expression, evalType) {
@@ -295,9 +409,163 @@ var expressionParser = {
     }
 };
 
+var expressionParser2 = {
+    init: function _init(expression, evalType) {
+        this.expression = expression;
+        this.stack = Object.create(stack).init();
+        this.evalType = evalType;
+        this.queue = [];
+    },
+    parse: function _parse() {
+        var re = /(&&|\|\||^|\)|\()/,
+            re2 = /(<|>|<=|>=|==|===|!=|!==)/,
+            nodes = this.expression.split(re),
+            operators = ['&&', '||', '!', '^'],
+            groupings = ['(', ')'],
+            opIdx = 0, grpIdx = 0,
+            curOp, teIdx;
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i] === '' || nodes[i] === ' ')
+                continue;
+            var token = nodes[i].trim();
+            if (opIdx = operators.indexOf(token) > -1) {
+                //TODO: compare operators precedence and associativity to top of stack
+                curOp = Object.create(operator).createNewOperator(operators[opIdx]);
+                while (this.stack.length()) {
+                    var stacktop = this.stack.peek();
+                    if ((stacktop.type === 'operator' && curOp.associtivity === 'R-T-L' && curOp.precedence <= stacktop.precedence)
+                        || (curOp.associtivity === 'L-T-R' && curOp.precedence < stacktop.precedence))
+                        this.queue.push(this.stack.pop());
+                    else
+                        break;
+                }
+                this.stack.push(curOp);
+            }
+            else if (grpIdx = groupings.indexOf(token) > -1) {
+                if (grpIdx === 0)
+                    this.stack.push(Object.create(operator).createNewOperator('('));
+                else {
+                    while (this.stack.peek().value !== '(') {
+                        this.queue.push(this.stack.pop());
+                    }
+                    this.stack.pop();   //pop off the actual '(' item but don't push it into the queue
+                }
+            }
+            else {  //if the token is not an operator and it's not a grouper, then it is an expression
+                if (this.evalType === 'post-evaluation') {
+
+                }
+                else {
+                    this.queue.push(evaluateExpression(token));
+                }
+            }
+
+        }
+
+        while (this.stack.length()) {
+            this.queue.push(this.stack.pop());
+        }
+    }
+};
+
+function buildTreeNode(expression) {
+    var re = /(<(?!=)|>(?!=)|<=|>=|==(?!=)|===|!=(?!=)|!==|!(?!==))/,
+        tokens = expression.split(re),
+        binaryOperators = ['<', '>', '<=', '>=', '==', '===', '!=', '!=='],
+        unaryOperators = ['!'],
+        token, val,
+        operator, isFirstOperandSet = false,
+        expressionObj = {
+            operator: null,
+            leftOperand: null,
+            rightOperand: null,
+            isLeftNegated: false,
+            isRightNegated: false
+        };
+
+    for (var i = 0; i < tokens.length; i++) {
+        token = tokens[i].trim();
+        if (token === '')
+            continue;
+
+        if (~binaryOperators.indexOf(token)) {
+            expressionObj.operator = token;
+        }
+        else if (~unaryOperators.indexOf(token)) {
+            if (!isFirstOperandSet)
+                expressionObj.isLeftNegated = true;
+            else
+                expressionObj.isRightNegated = true;
+        }
+        else {
+            val = [window].concat(token.split('.')).reduce(function findValueFromGlobal(prev, curr) {
+                return typeof prev === 'function' ? prev()[curr] : prev[curr];
+            });
+
+            if (typeof val !== 'function')
+                val = val();
+            else
+                val = val;
+
+            if (!isFirstOperandSet) {
+                expressionObj.leftOperand = val;
+                isFirstOperandSet = true;
+            }
+            else
+                expressionObj.rightOperand = val;
+        }
+    }
+}
+
 function evaluateExpression(expression) {
-    var re = /(<|>|<=|>=|==|===|!=|!==)/;
-    var tokens = expression.split(re);
+    var re = /(<(?!=)|>(?!=)|<=|>=|==(?!=)|===|!=(?!=)|!==|!(?!==))/,
+        tokens = expression.split(re),
+        binaryOperators = ['<', '>', '<=', '>=', '==', '===', '!=', '!=='],
+        unaryOperators = ['!'],
+        token, val,
+        operator, isFirstOperandSet = false,
+        expressionObj = {
+            operator: null,
+            leftOperand: null,
+            rightOperand: null,
+            isLeftNegated: false,
+            isRightNegated: false
+        };
+
+    for (var i = 0; i < tokens.length; i++) {
+        token = tokens[i].trim();
+        if (token === '')
+            continue;
+
+        if (~binaryOperators.indexOf(token)) {
+            expressionObj.operator = token;
+        }
+        else if (~unaryOperators.indexOf(token)) {
+            if (!isFirstOperandSet)
+                expressionObj.isLeftNegated = true;
+            else
+                expressionObj.isRightNegated = true;
+        }
+        else {
+            val = [window].concat(token.split('.')).reduce(function findValueFromGlobal(prev, curr) {
+                return typeof prev === 'function' ? prev()[curr] : prev[curr];
+            });
+
+            if (typeof val !== 'function')
+                val = val();
+            else
+                val = val;
+
+            if (!isFirstOperandSet) {
+                expressionObj.leftOperand = val;
+                isFirstOperandSet = true;
+            }
+            else
+                expressionObj.rightOperand = val;
+        }
+    }
+
     return comparator(tokens[0], tokens[1], (tokens[2] || ''));
 }
 
