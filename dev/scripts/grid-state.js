7d2a012ad4d34a77d256d725a413f293231af44d
mosbymc <mosbymc@gmail.com>
2016-09-27 22:15:50 -0400

    es6_conversion: added 'cacheStore' for defining and maintaining grid instance state cache

---
+++ dev/scripts/grid-state.js
@@ -0,0 +1,79 @@
+var gridCache = {
+    _cache:  []
+};
+
+Object.defineProperties(
+    gridCache, {
+        'getCache': {
+            value: function _getCache(id) {
+                return this._cache[id];
+            },
+            writable: false,
+            configurable: false
+        },
+        'deleteCache': {
+            value: function _deleteCache(id) {
+                delete this._cache[id];
+            },
+            writable: false,
+            configurable: false
+        }
+    }
+);
+
+var cache = [];
+
+var cacheStore = {
+    cache: {},
+    createNewGridCache: function _createNewGridCache(gridCacheObj) {
+        var newId = generateId();
+        cache[newId] = gridCacheObj;
+
+        Object.defineProperty(
+            cacheStore.cache,
+            newId.toString(),
+            {
+                get: function _get() {
+                    return cache[newId];
+                }
+            }
+        );
+
+        return newId;
+    },
+    deleteGridCache: function _deleteGridCache(cacheId) {
+        delete cacheStore.cache[cacheId];
+        delete cache[cacheId];
+    },
+    updateCacheWithPageData: function _updateCacheWithPageData(pageData, cacheId) {
+        var currCache = cache[cacheId];
+        currCache.dataSource.data = pageData.data;
+        currCache.pageSize = pageData.pageSize;
+        currCache.pageNum = pageData.pageNum;
+        currCache.dataSource.rowCount = pageData.rowCount != null ? pageData.rowCount : pageData.data.length;
+        currCache.groupedBy = pageData.groupedBy;
+        currCache.sortedOn = pageData.sortedOn;
+        currCache.filters = pageData.filters;
+    }
+};
+
+function createCache(cacheObj) {
+    var newId = generateId();
+    cache[newId] = cacheObj;
+    return newId;
+}
+
+function getCache(id) {
+    return cache[id];
+}
+
+function deleteCache(id) {
+    delete cache[id];
+}
+
+var generateId = (function guid(seed) {
+    return function _generateId() {
+        seed++;
+        return seed.toString();
+    };
+})(-1);
