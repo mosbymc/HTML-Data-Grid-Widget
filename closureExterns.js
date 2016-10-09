bceec5576a7ee98165412a2ff50864a46a8b10df
mosbymc <mosbymc@users.noreply.github.com>
2016-10-08 23:10:14 -0400

    Delete closureExterns.js

--- closureExterns.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var validator = {};
-validator.setAdditionalEvents = function(events) {};
-validator.validate = function(formElem) {};
-validator.removeErrors = function(elem) {};
-validator.validationRules = {};
-validator.inputEvents = [];
-
-var jQuery = {};
