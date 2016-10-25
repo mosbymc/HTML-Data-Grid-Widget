a32d142852658f386228c87c883694d6effeb99e
mosbymc <mosbymc@gmail.com>
2016-10-25 18:46:28 -0400

    develop: removed old/commented code

--- dev/scripts/grid.js
+++ dev/scripts/grid.js
@@ -1153,79 +1153,6 @@ var grid = (function _grid($) {
                     }
                 });
             });
-            /*for (i = 0; i < rowEnd; i++) {
-                gridData.dataSource.data[i]._initialRowIndex = i;
-                if (gridData.groupedBy && gridData.groupedBy.length) createGroupedRows(id, i, currentGroupingValues, contentTBody);
-
-                var tr = $('<tr class="data-row"></tr>').appendTo(contentTBody);
-                if (typeof gridData.parentGridId === 'number') tr.addClass('drill-down-row');
-                if (i % 2) {
-                    tr.addClass('alt-row');
-                    if (rows && rows.alternateRows && rows.alternateRows.constructor === Array)
-                        rows.alternateRows.forEach(function _addAlternateRowClasses(className) {
-                            tr.addClass(className);
-                        });
-                }
-
-                if (rows && rows.all && rows.all.constructor === Array) {
-                    rows.all.forEach(function _addRowClasses(className) {
-                        tr.addClass(className);
-                    });
-                }
-
-                if (gridData.groupedBy.length) {
-                    gridData.groupedBy.forEach(function _appendGroupingCells() {
-                        tr.append('<td class="grouped_cell">&nbsp</td>');
-                    });
-                }
-
-                if (gridData.drillDown)
-                    tr.append('<td class="drillDown_cell"><span class="drillDown_span" data-state="closed"><a class="drillDown-asc drillDown_acc"></a></span></td>');
-
-                gridData.columns.forEach(function _createGridCells(col) {
-                    var td = $('<td data-field="' + col.field + '" class="grid-content-cell"></td>').appendTo(tr);
-                    if (col.attributes && col.attributes.cellClasses && col.attributes.cellClasses.constructor === Array) {
-                        col.attributes.cellClasses.forEach(function _addColumnClasses(className) {
-                            td.addClass(className);
-                        });
-                    }
-                    if (col.type !== 'custom') {
-                        text = getFormattedCellText(col, gridData.dataSource.data[i][col.field]) || gridData.dataSource.data[i][col.field];
-                        text = text == null ? 'Null' : text;
-                        td.text(text);
-                    }
-                    else {
-                        td = col.html ? $(col.html).appendTo(td) : td;
-                        if (col.class)
-                            td.addClass(col.class);
-                        if (col.text) {
-                            var customText;
-                            if (typeof col.text === 'function') {
-                                col.text(gridData.originalData[gridData.dataSource.data[i]._initialRowIndex]);
-                            }
-                            else customText = col.text;
-                            td.text(customText);
-                        }
-                    }
-
-                    if (typeof col.events === 'object') {
-                        attachCustomCellHandler(col, td, id);
-                    }
-                    if (gridData.aggregates && gridData.aggregates[col.field] && typeof gridData.dataSource.get !== 'function') {
-                        if (gridData.pageRequest.eventType === 'filter' || gridData.pageRequest.eventType === undefined)
-                            addValueToAggregations(id, col.field, gridData.dataSource.data[i][col.field], gridData.gridAggregations);
-                    }
-                    //attach event handlers to save data
-                    if (typeof gridData.parentGridId !== 'number' && (col.editable && col.editable !== 'drop-down')) {
-                        makeCellEditable(id, td);
-                        gridState[id].editable = true;
-                    }
-                    else if (typeof gridData.parentGridId !== 'number' && (col.editable === 'drop-down')) {
-                        makeCellSelectable(id, td);
-                        gridState[id].editable = true;
-                    }
-                });
-            }*/
 
             gridData.columns.forEach(function appendCols() { colGroup.append('<col/>'); });
             gridData.groupedBy.forEach(function _prependCols() { colGroup.prepend('<col class="group_col"/>'); });
@@ -1329,21 +1256,6 @@ var grid = (function _grid($) {
                 else groupedDiff[idx] = 1;
             }
         });
-        /*for (j = 0; j < gridData.groupedBy.length; j++) {
-            //If the current cached value for the same field is different than the current grid's data for the same field,
-            //then cache the same value and note the diff.
-            if (!currentGroupingValues[gridData.groupedBy[j].field] || currentGroupingValues[gridData.groupedBy[j].field] !== gridData.dataSource.data[rowIndex][gridData.groupedBy[j].field]) {
-                currentGroupingValues[gridData.groupedBy[j].field] = gridData.dataSource.data[rowIndex][gridData.groupedBy[j].field];
-                groupedDiff[j] = 1;
-                foundDiff = true;
-            }
-            else {
-                //Otherwise, check the previous diff; if there isn't a diff, then set the current diff to none (i.e. 0),
-                //but if the previous diff was found, set the current diff.
-                if (!j || !groupedDiff[j - 1]) groupedDiff[j] = 0;
-                else groupedDiff[j] = 1;
-            }
-        }*/
         if (foundDiff && rowIndex && gridData.groupAggregates) {   //If a diff was found...
             groupedDiff.reverse().forEach(function _findRowDiffs(item, idx) {
                 var numItems = gridData.groupAggregations[idx]._items_; //...save the current row's number of items...
@@ -1374,35 +1286,6 @@ var grid = (function _grid($) {
                     }
                 }
             });
-            /*for (j = groupedDiff.length - 1; j >= 0; j--) {     //...go backwards through the grouped aggregates...
-                var numItems = gridData.groupAggregations[j]._items_; //...save the current row's number of items...
-                if (groupedDiff[j]) {                               //...if there is a diff at the current row, print it to the screen
-                    var groupAggregateRow = $('<tr class="grouped_row_header"></tr>').appendTo(gridContent);
-                    groupedDiff.forEach(function _appendGroupingCells() {
-                        groupAggregateRow.append('<td colspan="' + 1 + '" class="grouped_cell"></td>');
-                    });
-                    if (gridData.drillDown)
-                        groupAggregateRow.append('<td colspan="1" class="grouped_cell"></td>');
-                    gridData.columns.forEach(function _createAggregateCells(col) {
-                        if (col.field in gridData.groupAggregations[j] && col.field !== '_items_'){
-                            groupAggregateRow.append('<td class="group_aggregate_cell">' + (gridData.groupAggregations[j][col.field].text || '') + '</td>');
-                        }
-                        else
-                            groupAggregateRow.append('<td class="group_aggregate_cell"> </td>');
-                    });
-                    gridData.groupAggregations[j] = {       //Then reset the current row's aggregate object...
-                        _items_: 0
-                    };
-                    for (k = j - 1; k >= 0; k--) {  //...and go backward through the diffs starting from the prior index, and compare the number of items in each diff...
-                        if (groupedDiff[k] && gridData.groupAggregations[k]._items_ == numItems) {    //...if the number of items are equal, reset that diff as well
-                            groupedDiff[k] = 0;
-                            gridData.groupAggregations[k] = {
-                                _items_: 0
-                            };
-                        }
-                    }
-                }
-            }*/
         }
         groupedDiff.forEach(function _createGroupedAggregates(item, idx) {
             if (gridData.groupAggregates) {
